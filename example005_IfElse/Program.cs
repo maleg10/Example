fc980a419e55dd1de97844e6c181977da3065547
Oleg <maleg34@yandex.ru>
2023-05-21 15:18:23 +0300

    added folder 'example005_IfElse'

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать программу, которая будет приветствововать пользователя с определенным именем по особенному.
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
