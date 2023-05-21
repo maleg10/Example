e8b63cf7be3f2a9997cf2b08ccb86675f78fd616
Oleg <maleg34@yandex.ru>
2023-05-21 14:15:49 +0300

    added folder 'Example004_Div'

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿// написать программу на языке C# в которую пользователь вводит два целых числа и получает частное введеных чисел
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
